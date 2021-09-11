ea84306269ce6478e364e84dfdfe10e5d28accea
Senja <yauhen@uia.no>
2021-09-11 12:33:16 +0200

    Multiplication and division Operations work

--- main.c
+++ main.c
@@ -66,6 +66,8 @@ int peek(Stack stack)
 void enterValue(Stack , char );
 void AdditionOperation(Stack);
 void SubtractionOperation(Stack);
+void MultiplicationOperation(Stack);
+void DivisionOperation(Stack);
 
 int main ()
 {
@@ -82,17 +84,25 @@ int main ()
         {
             break;
         }
-        else if(userInput > 47 && userInput < 58)
+        else if(userInput > 47 && userInput < 58) // if a number
         {
             enterValue(stack,  userInput);
         }
-        else if(userInput == 43)
+        else if(userInput == 43) // addition
         {
             AdditionOperation(stack);
         }
-        else if (userInput == 45)
+        else if (userInput == 45) // subtraction
         {
             SubtractionOperation(stack);
+        }
+        else if(userInput == 42 || userInput == 120) // multiplication
+        {
+            MultiplicationOperation(stack);
+        }
+        else if (userInput == 47 || userInput == 58)
+        {
+            DivisionOperation(stack);
         }
         else
         {
@@ -105,7 +115,7 @@ int main ()
 
 void enterValue(Stack stack, char value)
 {
-    int IntValue = value - 48;
+    int IntValue = value - 48; // from char to int
 
     if(!isFull(stack))
         push(stack, IntValue);
@@ -118,26 +128,28 @@ void AdditionOperation(Stack stack)
     int firstOperand = 0;
     int secondOperand = 0;
 
-    if(peek(stack) != EmptyTos)
+    if(peek(stack) != EmptyTos) // if stack is not empty
     {
-        firstOperand = pop(stack);
+        firstOperand = pop(stack); // remove first value and store it in a variable
 
-        if(peek(stack) != EmptyTos)
+        if(peek(stack) != EmptyTos)  // if stack is still not empty
         {
-            secondOperand = pop(stack);
+            secondOperand = pop(stack); // store and remove the second value
         }
-        else
+        else // if there are no second operand first value will be returned and user will get error message
         {
             push(stack, firstOperand);
             printf("Not enough values to perform addition on\n");
+            firstOperand = 0; // null out value, just in case
         }
     }
-    else
+    else // if stack is empty to begin with, user will be the message
     {
         printf("No values to perform addition on\n");
+        printf("looks like no values have been entered\n");
     }
 
-    if(firstOperand && secondOperand)
+    if(firstOperand && secondOperand) // if both values are not zero, operations will be performed
     {
         int sumOfOperands = firstOperand + secondOperand;
         printf("\n");
@@ -162,6 +174,7 @@ void SubtractionOperation(Stack stack)
         {
             push(stack, firstOperand);
             printf("Not enough values to subtract\n");
+            firstOperand = 0; // null out value, just in case
         }
     }
     else
@@ -175,4 +188,95 @@ void SubtractionOperation(Stack stack)
         printf("\n");
         push(stack, DifferenceOfOperands);
     }
+}
+
+void MultiplicationOperation(Stack stack)
+{
+    int firstOperand = 0;
+    int secondOperand = 0;
+
+    if(peek(stack) != EmptyTos)
+    {
+        firstOperand = pop(stack);
+
+        if(peek(stack) != EmptyTos)
+        {
+            secondOperand = pop(stack);
+
+        } else
+        {
+            push(stack, firstOperand);
+            printf("Not enough values to subtract\n");
+            firstOperand = 0; // null out value, just in case
+        }
+    }
+    else
+    {
+        printf("No values to perform subtraction on\n");
+    }
+
+    if(firstOperand && secondOperand)
+    {
+        int ProductOfOperands = firstOperand * secondOperand;
+        printf("\n");
+        push(stack, ProductOfOperands);
+    }
+}
+
+void DivisionOperation(Stack stack)
+{
+    int numerator = 0;
+    int denominator = 0;
+
+    if(peek(stack) != EmptyTos)
+    {
+        denominator = pop(stack);
+
+        if(peek(stack) != EmptyTos || peek(stack) != 0)
+        {
+            numerator = pop(stack);
+        }
+        else
+        {
+            printf("hello");
+            push(stack, numerator);
+            printf("Not sufficient values to subtract\n");
+            numerator = 0; // null out value, just in case
+        }
+    }
+    else
+    {
+        printf("No values to perform subtraction on\n");
+    }
+
+    if(numerator && denominator)
+    {
+        if(numerator%denominator == 0)
+        {
+            int QuotientOfOperand = numerator / denominator;
+            printf("\n");
+            push(stack, QuotientOfOperand);
+        }
+        else if(numerator%denominator != 0)
+        {
+            int mod;
+            int gcd;
+
+            // Increasing the number till the rest of division both numerator and denominator
+            // The for-loop will go on until mod in larger og equal to both numerator & denominator
+
+            for(mod=1; mod <= numerator && mod <= denominator; ++mod)
+            {
+                // Checks if mod is factor of both integers
+                if(numerator % mod == 0 && denominator % mod == 0)
+                    gcd = mod; // The number that fills the requirement becomes gcd
+            }
+
+            printf("%i/%i\n", numerator/gcd, denominator/gcd);
+            printf("This is not an integer and therefore cannot be pushed to the stack\n");
+
+        }
+
+    }
+
 }
